dc05ed6ab0aae9981c8ad521dd28d996ebafb72f
kenans <kenansun0@gmail.com>
2014-06-26 15:08:22 +0200

    [26/06/2014] All the other usb_manager compile options removed.

--- App_Sources/project_pilote/usb/usb_manager.c
+++ App_Sources/project_pilote/usb/usb_manager.c
@@ -53,16 +53,6 @@ static uint8_t _start = 0;                          // start id
 static uint8_t _end = 0;                            // end id
 // ---------------------------------------------------------------------------------------------------
 /**
- * Compile options
- *      -1: Use new frame with ring buffer
- *      1 : Use old frame but no ring buffer
- */
-#define USB_OLD_FRAME   -1
-#if USB_OLD_FRAME==1
-static uint8_t              _temp[16];
-#endif  // if USB_OLD_FRAME==1
-
-/**
  * USB_Task
  * 		- A USB CDC Device task
  */
@@ -103,7 +93,6 @@ void USB_Task(void *pvParameters)
             xQueueSend(mbox_pilote_recv, &mes_pkg_recv, 0);
         }
 
-#if USB_OLD_FRAME==-1
         // Every 50ms, if Rx not empty, copy all the items from Rx to local USB buffer;
         if (CDC1_GetCharsInRxBuf() != 0) {
             err_t error = ERR_OK;
@@ -195,111 +184,6 @@ void USB_Task(void *pvParameters)
             // If USB connected but no data transfered
             vTaskDelay(50/portTICK_PERIOD_MS);
         }
-#elif USB_OLD_FRAME==1
-        if (CDC1_GetCharsInRxBuf() != 0) {                                  // If there is some data transfered
-            uint8_t i = 0;
-            bool in_buf_not_empty = FALSE;
-            for (i = 0; i < sizeof(_local_buf); i++) {                      // Copy them to a local buffer
-                if (CDC1_GetChar(&_local_buf[i]) == ERR_OK) {
-                    in_buf_not_empty = TRUE;
-                } else {
-                    break;
-                }
-            }
-
-            if (in_buf_not_empty) {                                         // Does the local buffer have some data?
-                // Is the received frame valid?
-                for (i = 0; i < sizeof(_local_buf)-9; i++) {
-                    if (_local_buf[i+0] == '$' &&
-                        _local_buf[i+2] == '$' &&
-                        _local_buf[i+4] == '$' &&
-                        _local_buf[i+9] == '$') {
-                        if (_local_buf[i+1] == '0') {
-                            // Load configuration
-                            // TODO
-                            (void)CDC1_SendString((unsigned char*)"echo: 0\n");
-                            // First prepare message package for requiring configuration
-                            mes_pkg_recv.direction = PILOTE_MES_RECV;
-                            mes_pkg_recv.mes_type  = PILOTE_MES_TYPE_USB;
-                            mes_pkg_recv.operation = PILOTE_MES_OPERATION_READ_CONFIG;
-                            // Then send message package to mbox_recv
-                            xQueueSend(mbox_pilote_recv, &mes_pkg_recv, MBOX_TIMEOUT_50MS);
-                            // Wait for the response from mbox_send
-                            xQueueReceive(mbox_pilote_send, &mes_pkg_send, MBOX_TIMEOUT_INFINIT);
-                            // Verify the got message
-                            if (mes_pkg_send.direction == PILOTE_MES_SEND &&
-                                mes_pkg_send.mes_type  == PILOTE_MES_TYPE_USB &&
-                                mes_pkg_send.operation == PILOTE_MES_OPERATION_REPLY_CONFIG) {
-                            // Parse message package in mbox_send, then send back to PC
-                                PiloteConfigurations *pilote_config =
-                                        (PiloteConfigurations*)mes_pkg_send.data_ptr;
-                                _temp[15] = '\0';
-                                (void)CDC1_SendString((unsigned char*)"Enable:");
-                                UTIL_Num8uToStr(_temp, sizeof(_temp), pilote_config->enabled);
-                                (void)CDC1_SendString((unsigned char*)_temp);
-                                (void)CDC1_SendString((unsigned char*)"\n");
-
-                                (void)CDC1_SendString((unsigned char*)"Code:");
-                                UTIL_Num16uToStr(_temp, sizeof(_temp), pilote_config->code);
-                                (void)CDC1_SendString((unsigned char*)_temp);
-                                (void)CDC1_SendString((unsigned char*)"\n");
-
-                                (void)CDC1_SendString((unsigned char*)"Time Bt Frames:");
-                                UTIL_Num32sToStr(_temp, sizeof(_temp), pilote_config->time_between_frames);
-                                (void)CDC1_SendString((unsigned char*)_temp);
-                                (void)CDC1_SendString((unsigned char*)"\n");
-
-                                (void)CDC1_SendString((unsigned char*)"Nums of Frames:");
-                                UTIL_Num8uToStr(_temp, sizeof(_temp), pilote_config->nums_of_frames);
-                                (void)CDC1_SendString((unsigned char*)_temp);
-                                (void)CDC1_SendString((unsigned char*)"\n");
-                            }
-                        } else if (_local_buf[i+1] == '1') {
-                            // Modify configuration
-                            // TODO
-                            (void)CDC1_SendString((unsigned char*)"echo: 1\n");
-                            // First prepare message package for modifying data
-                            mes_pkg_recv.direction = PILOTE_MES_RECV;
-                            mes_pkg_recv.mes_type  = PILOTE_MES_TYPE_USB;
-                            mes_pkg_recv.operation = PILOTE_MES_OPERATION_MODIFY;
-                            // Then parse target and data, and add it to message package
-                            if (_local_buf[i+3] == '0') {
-                                mes_pkg_recv.target = PILOTE_MES_TARGET_ENABLE;
-                                (_local_buf[i+8]=='1')
-                                      ? (mes_pkg_recv.data = 1)
-                                      : (mes_pkg_recv.data = 0);
-                            } else if (_local_buf[i+3] == '1') {
-                                mes_pkg_recv.target = PILOTE_MES_TARGET_CODE;
-                                mes_pkg_recv.data =
-                                        (_local_buf[i+7] - '0') * 10u +
-                                        (_local_buf[i+8] - '0');
-                            } else if (_local_buf[i+3] == '2') {
-                                mes_pkg_recv.target = PILOTE_MES_TARGET_TIME_BT_FRAMES;
-                                mes_pkg_recv.data =
-                                        (_local_buf[i+5] - '0') * 1000u +
-                                        (_local_buf[i+6] - '0') * 100u +
-                                        (_local_buf[i+7] - '0') * 10u +
-                                        (_local_buf[i+8] - '0');
-                            } else if (_local_buf[i+3] == '3') {
-                                mes_pkg_recv.target = PILOTE_MES_TARGET_NUMS_OF_FRAMES;
-                                mes_pkg_recv.data =
-                                        (_local_buf[i+7] - '0') * 10u +
-                                        (_local_buf[i+8] - '0');
-                            }
-                            // Send the message package to mbox_recv
-                            xQueueSend(mbox_pilote_recv, &mes_pkg_recv, MBOX_TIMEOUT_50MS);
-                        }
-                        break;
-                    }
-                }
-                // Clear input buffer
-                for (i = 0; i < sizeof(_local_buf); i++) {
-                    _local_buf[i] = 0;
-                }
-                in_buf_not_empty = FALSE;
-            }
-        }
-#endif  // if USB_OLD_FRAME==0
     }
 }
 
